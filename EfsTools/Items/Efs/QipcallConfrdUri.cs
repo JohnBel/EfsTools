8dee44dd0d45a65b0eac7f850de4a19ea224c507
JohnBel <johnbel@narod.ru>
2019-04-05 09:22:10 +0300

    Исправлено имя класса QipcallConfrdUri

--- EfsTools/Items/Efs/QipcallConfrdUri.cs
+++ EfsTools/Items/Efs/QipcallConfrdUri.cs
@@ -7,7 +7,7 @@ namespace EfsTools.Items.Efs
     [Serializable]
     [EfsFile("/nv/item_files/ims/qipcall_confrd_uri", true, 0xE1FF)]
     [Attributes(9)]
-    public class QipcallConfrdUr
+    public class QipcallConfrdUri
     {
         [ElementsCount(1)]
         [ElementType("uint16")]
